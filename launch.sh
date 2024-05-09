e953ac7b23751ccc83a9de11542175912b6bcafe
Ninoh-FOX <ninohfox@gmail.com>
2024-05-09 11:57:38 +0200

    initial commit

---
+++ launch.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+echo $0 $*
+cd $(dirname "$0")
+
+cpuclock performance 2 1200 0
+
+HOME=/mnt/SDCARD/App/gmu
+export LD_LIBRARY_PATH=./lib:$LD_LIBRARY_PATH
+
+SDL_NOMOUSE=1 SDL_VIDEODRIVER=a30  ./gmu.bin -c gmu.miyoo.conf &> log.txt
+sync
